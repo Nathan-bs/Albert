14c75ba90904d3a77077056ec7cd1d54e901857a
lunat <lunat@172.29.13.22>
2023-09-05 08:56:29 -0300

    First

---
+++ Albert/src/First/MPP.java
@@ -0,0 +1,19 @@
+package First;
+
+public class MPP {
+			private String marca;
+			private String cor;
+			private int numPortas;
+			private int ano;
+			private boolean movimento;
+			public MPP(String marca, String cor, int numPortas, int ano, boolean movimento) {
+				super();
+				this.marca = marca;
+				this.cor = cor;
+				this.numPortas = numPortas;
+				this.ano = ano;
+				this.movimento = movimento;
+			} 
+		
+			
+	}
